96d30856ec68636937f1d8ec26829989af4194a8
eRqa <ivlasov.spb@gmail.com>
2020-10-17 15:27:22 +0300

    Реализован класс Jukebox и метод исполнения выбранной песни

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+
+    public void music(int position) {
+        switch (position) {
+            case 1:
+                System.out.println("Пусть бегут неуклюже");
+                break;
+            case 2:
+                System.out.println("Спокойной ночи");
+                break;
+            default:
+                System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox jb = new Jukebox();
+        jb.music(1);
+        jb.music(2);
+        jb.music(3);
+    }
+
+}
--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -3,16 +3,12 @@ package ru.job4j.oop;
 public class Student {
     public static void main(String[] args) {
         Student petya = new Student();
-        petya.music();
-        petya.song();
-        petya.music();
-        petya.song();
-        petya.music();
-        petya.song();
+        String song = "I believe, I can fly";
+        petya.music(song);
     }
 
-    public void music() {
-        System.out.println("Tra tra tra");
+    public void music(String lyrics) {
+        System.out.println("I can sign a song : " + lyrics);
     }
 
     public void song() {
