7975bdc116c9ce5e194086353e168bbddc70e062
eRqa <ivlasov.spb@gmail.com>
2020-10-20 23:54:20 +0300

    Реализованы конструкторы класс Item

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,8 +3,22 @@ package ru.job4j.tracker;
 import java.time.LocalDateTime;
 
 public class Item {
+
     private int id;
     private String name;
+
+    public Item() {
+
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public LocalDateTime getCreated() {
         return created;
