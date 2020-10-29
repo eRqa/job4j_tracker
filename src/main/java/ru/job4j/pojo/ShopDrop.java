ee79ebdfb3fbc88d38a2758269e8ac3348dfa6a7
eRqa <ivlasov.spb@gmail.com>
2020-10-29 22:50:42 +0300

    Оптимизирован метод удаления элемента из массива. Условие проверки конечности вынесено за цикл

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -3,13 +3,11 @@ package ru.job4j.pojo;
 public class ShopDrop {
     public static Product[] leftShift(Product[] products, int index) {
 
-        for (int i = index; i < products.length; i++) {
-            if (i + 1 == products.length) {
-                products[i] = null;
-            } else {
-                products[i] = products[i + 1];
-            }
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
         }
+
+        products[products.length - 1] = null;
 
         return products;
     }
