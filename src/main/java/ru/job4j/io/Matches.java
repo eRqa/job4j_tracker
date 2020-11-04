439dc8508edf14c96e1181903a8e76578cf72f68
eRqa <ivlasov.spb@gmail.com>
2020-11-04 20:37:22 +0300

    Реализована игра 11

---
+++ src/main/java/ru/job4j/io/Matches.java
@@ -0,0 +1,39 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Matches {
+
+    public static void main(String[] args) {
+
+        int matchesCount = 11;
+        boolean run = true;
+        Scanner sc = new Scanner(System.in);
+
+        while (run) {
+
+            System.out.println("Введите количество списек от 1 до 3");
+            int select = Integer.parseInt(sc.nextLine());
+
+            if (select < 1 || select > 3) {
+                System.out.println("Выбрать число можно только из диапозона от 1 до 3");
+                continue;
+            }
+
+            if (matchesCount - select < 0) {
+                System.out.println("Нельзя взять спичек больше чем лежит на столе");
+                continue;
+            }
+
+            matchesCount = matchesCount - select;
+            System.out.println("На столе осталось " + matchesCount + " спичек");
+
+            if (matchesCount == 0) {
+                run = false;
+            }
+
+        }
+
+    }
+
+}
