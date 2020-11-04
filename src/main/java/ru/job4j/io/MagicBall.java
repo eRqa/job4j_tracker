60bd8f3114e0599643c9479837476159310749e2
eRqa <ivlasov.spb@gmail.com>
2020-11-04 14:13:48 +0300

    Магический шар

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicBall {
+
+    public static void main(String[] args) {
+        System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+        Scanner sc = new Scanner(System.in);
+        String question = sc.nextLine();
+        int answer = new Random().nextInt(3);
+        switch (answer) {
+            case 0 :
+                System.out.println("Да");
+                break;
+            case 1 :
+                System.out.println("Нет");
+                break;
+            default :
+                System.out.println("Может быть");
+                break;
+        }
+    }
+
+}
