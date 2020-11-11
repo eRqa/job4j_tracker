71a7506caa7fd6fc83a3beb51f5f03b4089b0e40
eRqa <ivlasov.spb@gmail.com>
2020-11-11 23:17:06 +0300

    Функциональность вынесена в статические методы

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,71 +8,94 @@ public class StartUI {
             this.showMenu();
             int select = Integer.parseInt(input.askStr("Select: "));
             if (select == 0) {
-                System.out.println("=== Create a new Item ====");
-                String msg = "Enter name: ";
-                String name = input.askStr(msg);
-                Item item = new Item(name);
-                tracker.add(item);
+                createItem(tracker, input);
             }  else if (select == 1) {
-                Item[] allItems = tracker.findAll();
-
-                if (allItems.length > 0) {
-                    for (Item curItem:
-                            allItems) {
-                        System.out.println(curItem.toString());
-                    }
-                } else {
-                    System.out.println("No one item found");
-                }
-
+                showAllItems(tracker);
             } else if (select == 2) {
-                System.out.println("=== Edit item ===");
-                String msg = "Enter id";
-                int itemId = Integer.parseInt(input.askStr(msg));
-                System.out.println("Enter name");
-                String name = input.askStr(msg);
-                Item newItem = new Item(name);
-                if (tracker.replace(itemId, newItem)) {
-                    System.out.println("Successful change item");
-                } else {
-                    System.out.println("Failed change");
-                }
+                replaceItem(tracker, input);
             } else if (select == 3) {
-                System.out.println("=== Delete item ===");
-                String msg = "Enter id";
-                int itemId = Integer.parseInt(input.askStr(msg));
-                if (tracker.delete(itemId)) {
-                    System.out.println("Successful delete");
-                } else {
-                    System.out.println("Failed delete");
-                }
+                deleteItem(tracker, input);
             } else if (select == 4) {
-                System.out.println("=== Find item by id ===");
-                String msg = "Enter id";
-                int itemId = Integer.parseInt(input.askStr(msg));
-                Item foundItem = tracker.findById(itemId);
-                if (foundItem != null) {
-                    System.out.println(foundItem.toString());
-                } else {
-                    System.out.println("No items with this id found");
-                }
+                findItemById(tracker, input);
             } else if (select == 5) {
-                System.out.println("=== Find items by name ===");
-                String msg = "Enter name";
-                String name = input.askStr(msg);
-                Item[] arrItems = tracker.findByName(name);
-                if (arrItems.length > 0) {
-                    System.out.println(arrItems.length + " items found");
-                    for (Item curItem :
-                            arrItems) {
-                        System.out.println(curItem.toString());
-                    }
-                } else {
-                    System.out.println("no items found");
-                }
+                findItemByName(tracker, input);
             } else if (select == 6) {
                 run = false;
             }
+        }
+    }
+
+    public static void findItemByName(Tracker tracker, Input input) {
+        System.out.println("=== Find items by name ===");
+        String msg = "Enter name";
+        String name = input.askStr(msg);
+        Item[] arrItems = tracker.findByName(name);
+        if (arrItems.length > 0) {
+            System.out.println(arrItems.length + " items found");
+            for (Item curItem :
+                    arrItems) {
+                System.out.println(curItem.toString());
+            }
+        } else {
+            System.out.println("no items found");
+        }
+    }
+
+    public static void showAllItems(Tracker tracker) {
+        Item[] allItems = tracker.findAll();
+
+        if (allItems.length > 0) {
+            for (Item curItem:
+                    allItems) {
+                System.out.println(curItem.toString());
+            }
+        } else {
+            System.out.println("No one item found");
+        }
+    }
+
+    public static void findItemById(Tracker tracker, Input input) {
+        System.out.println("=== Find item by id ===");
+        String msg = "Enter id";
+        int itemId = Integer.parseInt(input.askStr(msg));
+        Item foundItem = tracker.findById(itemId);
+        if (foundItem != null) {
+            System.out.println(foundItem.toString());
+        } else {
+            System.out.println("No items with this id found");
+        }
+    }
+
+    public static void createItem(Tracker tracker, Input input) {
+        System.out.println("=== Create a new Item ====");
+        String msg = "Enter name: ";
+        String name = input.askStr(msg);
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+
+    public static void replaceItem(Tracker tracker, Input input) {
+        System.out.println("=== Edit item ===");
+        String msg = "Enter id";
+        int itemId = Integer.parseInt(input.askStr(msg));
+        System.out.println("Enter name");
+        String name = input.askStr(msg);
+        Item newItem = new Item(name);
+        if (tracker.replace(itemId, newItem)) {
+            System.out.println("Successful change item");
+        } else {
+            System.out.println("Failed change");
+        }
+    }
+
+    public static void deleteItem(Tracker tracker, Input input) {
+        System.out.println("=== Delete item ===");
+        String msg = "Enter id";
+        int itemId = Integer.parseInt(input.askStr(msg));
+        if (tracker.delete(itemId)) {
+            System.out.println("Successful delete");
+        } else {
+            System.out.println("Failed delete");
         }
     }
 
