49a1805160ae78d9f85a8f81be35b82a390cfa29
eRqa <ivlasov.spb@gmail.com>
2020-11-12 20:48:51 +0300

    Реализован пример шаблона проектирования стратегия

---
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -0,0 +1,17 @@
+package ru.job4j.strategy;
+
+public class Square implements Shape {
+
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return    "_________" + ln
+                + "|   :)  |" + ln
+                + "|_______|" + ln;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Triangle().draw());
+    }
+
+}
