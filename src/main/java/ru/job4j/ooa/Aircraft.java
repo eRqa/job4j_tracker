90590c776bfe3b6222a834479dc7fd99595e1ede
eRqa <ivlasov.spb@gmail.com>
2020-11-30 22:00:11 +0300

    Исправлены ошибки компиляции

---
+++ src/main/java/ru/job4j/ooa/Aircraft.java
@@ -0,0 +1,11 @@
+package ru.job4j.ooa;
+
+public class Aircraft {
+
+    //Разрешим переопределение убрав final
+//    public final void printModel() {
+    public void printModel() {
+        System.out.println("Метод выводит в консоль модель самолета.");
+    }
+
+}
---
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -0,0 +1,17 @@
+package ru.job4j.ooa;
+
+public class Airport {
+    public static void main(String[] args) {
+        final Airbus airbus = new Airbus("A320");
+        System.out.println(airbus);
+        airbus.printModel();
+        airbus.printCountEngine();
+
+//        airbus = new Airbus("A380"); airbus - final. Можем изменить состояние, но не можем изменить ссылку
+        airbus.setName("A380");
+        System.out.println(airbus);
+
+        airbus.setName("A380");
+        System.out.println(airbus);
+    }
+}
