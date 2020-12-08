5e9528a86a6ba027b1a21d3f098b38cea5718aa7
eRqa <ivlasov.spb@gmail.com>
2020-12-08 22:02:34 +0300

    Разработка метода конвертации List в HashMap

---
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class OrderConvertTest {
+    @Test
+    public void whenSingleOrder() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3sfe", "Dress"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
+    }
+
+    @Test
+    public void whenThreeOrders() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3sfe", "Dress"));
+        orders.add(new Order("4a-ge", "T-Shirt"));
+        orders.add(new Order("1jz-gte", "Socks"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
+        assertThat(map.get("4a-ge"), is(new Order("4a-ge", "T-Shirt")));
+        assertThat(map.get("1jz-gte"), is(new Order("1jz-gte", "Socks")));
+    }
+}
