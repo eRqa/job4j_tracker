dd46150023f837f78b153da7ac7fb63f7f5dcadb
eRqa <ivlasov.spb@gmail.com>
2020-12-14 23:39:49 +0300

    Разработаны классы для сортировки Item по наименованию

---
+++ src/test/java/ru/job4j/tracker/SortSortItemByNameASCTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SortSortItemByNameASCTest {
+
+    @Test
+    public void whenCompareWithThreeItems() {
+        List<Item> items = Arrays.asList(
+                new Item(1, "third"),
+                new Item(2, "second"),
+                new Item(3, "first")
+        );
+        Collections.sort(items, new SortSortItemByNameASC());
+        assertThat(items.get(0).getName(), is("first"));
+        assertThat(items.get(1).getName(), is("second"));
+        assertThat(items.get(2).getName(), is("third"));
+    }
+
+    @Test
+    public void whenCompare() {
+        List<Item> items = Arrays.asList(
+                new Item(1, "ABC"),
+                new Item(2, "ZZZ"),
+                new Item(3, "DEF")
+        );
+        Collections.sort(items, new SortSortItemByNameASC());
+        assertThat(items.get(0).getName(), is("ABC"));
+        assertThat(items.get(1).getName(), is("DEF"));
+        assertThat(items.get(2).getName(), is("ZZZ"));
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/SortSortItemByNameDESCTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SortSortItemByNameDESCTest {
+
+    @Test
+    public void whenCompareWithThreeItems() {
+        List<Item> items = Arrays.asList(
+                new Item(1, "third"),
+                new Item(2, "second"),
+                new Item(3, "first")
+        );
+        Collections.sort(items, new SortSortItemByNameDESC());
+        assertThat(items.get(0).getName(), is("third"));
+        assertThat(items.get(1).getName(), is("second"));
+        assertThat(items.get(2).getName(), is("first"));
+    }
+
+    @Test
+    public void whenCompare() {
+        List<Item> items = Arrays.asList(
+                new Item(1, "ABC"),
+                new Item(2, "ZZZ"),
+                new Item(3, "DEF")
+        );
+        Collections.sort(items, new SortSortItemByNameDESC());
+        assertThat(items.get(0).getName(), is("ZZZ"));
+        assertThat(items.get(1).getName(), is("DEF"));
+        assertThat(items.get(2).getName(), is("ABC"));
+    }
+
+}
