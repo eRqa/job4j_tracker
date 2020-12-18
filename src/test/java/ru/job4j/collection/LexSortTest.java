7266abcadbb9126d05fb709aa735dab43f4f4615
eRqa <ivlasov.spb@gmail.com>
2020-12-18 20:20:53 +0300

    Реализован метод сортировки задач по номеру

---
+++ src/test/java/ru/job4j/collection/LexSortTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.collection;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+import java.util.Arrays;
+
+public class LexSortTest {
+    @Test
+    public void sortNum1and2and10() {
+        String[] input = {
+                "10. Task.",
+                "1. Task.",
+                "2. Task."
+        };
+        String[] out = {
+                "1. Task.",
+                "2. Task.",
+                "10. Task."
+        };
+        Arrays.sort(input, new LexSort());
+        assertThat(input, is(out));
+    }
+
+    @Test
+    public void sort() {
+        String[] input = {
+                "10. Task.",
+                "1. Task.",
+                "4. Task.",
+                "6. Task.",
+                "2. Task.",
+                "11. Task."
+        };
+        String[] out = {
+                "1. Task.",
+                "2. Task.",
+                "4. Task.",
+                "6. Task.",
+                "10. Task.",
+                "11. Task."
+        };
+        Arrays.sort(input, new LexSort());
+        assertThat(input, is(out));
+    }
+}
