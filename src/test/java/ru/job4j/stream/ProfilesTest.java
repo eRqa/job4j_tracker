5eb30024e28443889a577e5437dc73177bb0f641
eRqa <ivlasov.spb@gmail.com>
2020-12-24 00:25:10 +0300

    Извелчение списка адресов при помощи Stream map

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.stream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+
+    @Test
+    public void whenCollect() {
+        List<Profile> profiles = new ArrayList<>();
+        Address address1 = new Address("Moscow",
+                                    "Leninskaya",
+                                    3,
+                                    3);
+        Address address2 = new Address("SPb",
+                                    "Leninskaya",
+                                    2,
+                                    2);
+        Address address3 = new Address("Omsk",
+                                    "Leninskaya",
+                                    4,
+                                    4);
+        profiles.add(new Profile(address1));
+        profiles.add(new Profile(address2));
+        profiles.add(new Profile(address3));
+
+        Profiles profCollect = new Profiles();
+        List<Address> act = profCollect.collect(profiles);
+
+        List<Address> expected = Arrays.asList(address1,
+                                                address2,
+                                                address3);
+
+        assertThat(act, is(expected));
+
+    }
+}
