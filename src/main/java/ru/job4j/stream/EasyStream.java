bfc96f5a469809c0400136462239e188a5512d89
eRqa <ivlasov.spb@gmail.com>
2021-02-06 12:25:34 +0300

    Реализована дополнительная задача по блоку FP, Lambda, StreamAPI

---
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -0,0 +1,35 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EasyStream {
+
+    private List<Integer> data;
+
+    public static EasyStream of(List<Integer> source) {
+        EasyStream easyStream = new EasyStream();
+        easyStream.data = new ArrayList<>(source);
+        return easyStream;
+    }
+
+    public EasyStream map(Function<Integer, Integer> fun) {
+        List<Integer> tmpList = new ArrayList<>();
+        for (Integer value : data) {
+            tmpList.add(fun.apply(value));
+        }
+        data = tmpList;
+        return this;
+    }
+
+    public EasyStream filter(Predicate<Integer> fun) {
+        data.removeIf(value -> !fun.test(value));
+        return this;
+    }
+
+    public List<Integer> collect() {
+        return this.data;
+    }
+}
