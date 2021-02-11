97dcbbe3e8cc1f3b448e89a6ffd6af6c9f7767c2
eRqa <ivlasov.spb@gmail.com>
2021-02-11 20:49:13 +0300

    Написаны SQL скрипты инициализации таблицы Items и начального заполнения

---
+++ src/main/java/ru/job4j/db/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO ITEMS (NAME) VALUES
+('Создать таблицу Items в базе данных Tracker');
+INSERT INTO ITEMS (NAME) VALUES
+('Заполнить начальыне данные в таблице Items');
+INSERT INTO ITEMS (NAME) VALUES
+('Реализовать механизм получения заявое из базы данных Tracker');
